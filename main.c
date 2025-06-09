52b8f34ef654c5bd59c0b8337f25fd1fd34489c7
Kutukobra <nadzhiff@gmail.com>
2025-06-09 11:35:51 +0700

    Readme selesai

--- main.c
+++ main.c
@@ -42,6 +42,7 @@ double regressionSlope(double x[], double y[], int n) {
 }
 
 double calculateConductivity(double b) {
+    // Konstanta eksperimen
     static const double m = 4.3e-3, x = 3.8e-3, A = 7.76e-3, c = 900, T1 = 26.2, T2 = 49.6;
 
     return (m * c * x) / (A * (T2 - T1)) * b;
@@ -58,7 +59,7 @@ double plotCSV(FILE *input, double x[], double y[], double b, int count) {
     fprintf(pout, "dt,dT,y_fit\n");
     for (int i = 0; i < count; i++) {
         double dt = x[i], dT = y[i];
-        double y_fit = b * dt;
+        double y_fit = b * dt; // y fit buat aproksimasi berdasarkan regresi
         fprintf(pout, "%lf,%lf,%lf\n", dt, dT, y_fit);
     }
     fclose(pout);
@@ -70,15 +71,16 @@ int main() {
         printf("Error: Cannot open dataset.csv\n");
         return 1;
     }
-    
-
     int count = fileLineCount(input);
 
+    // Alokasi memori untuk x y
     double *x = malloc(sizeof(double) * count);
     double *y = malloc(sizeof(double) * count); 
     
     char header[100];
     fgets(header, sizeof(header), input);
+
+    // Input setiap dt dan dT
     for (int i = 0; i < count; i++)     {
         if (fscanf(input, "%lf,%lf", &y[i], &x[i]) != 2) {
             printf("Error reading dataset %d\n", i);
